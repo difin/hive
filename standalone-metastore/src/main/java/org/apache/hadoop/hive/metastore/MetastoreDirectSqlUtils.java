6df8dad0ca008adaf00eb9e7d68f2db2f722566e
Vineet Garg <vgarg@apache.org>
2018-11-09 14:11:51 -0800

    CDPD-15291: Partial backport of HIVE-20833 package.jdo needs to be updated to conform with HIVE-20221 changes (Vineet Garg, reviewed by Jason Dere)
    (cherry picked from commit 7ae4a2c81a7a3a22ab2df2c5f75638e3f5e580f0)
    Change-Id: Ic94a7a39fa7e9dd2b025f50aa43018db513bf9c4
    (cherry picked from commit b8cb3f4d3a492af939ed8d85c9cde48fa37058cf)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/MetastoreDirectSqlUtils.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/MetastoreDirectSqlUtils.java
@@ -201,7 +201,7 @@ class MetastoreDirectSqlUtils {
     loopJoinOrderedResult(pm, partitions, queryText, queryParams.toArray(), 0, new ApplyFunc<Partition>() {
       @Override
       public void apply(Partition t, Object[] fields) {
-        t.putToParameters((String) fields[1], (String) fields[2]);
+        t.putToParameters((String) fields[1], extractSqlClob(fields[2]));
       }
     });
     // Perform conversion of null map values
