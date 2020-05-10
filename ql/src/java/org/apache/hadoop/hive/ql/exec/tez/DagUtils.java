81d544a1ba9b71d9fdb48e67676f060d0c389f71
Ashutosh Chauhan <hashutosh@apache.org>
2020-05-09 18:01:36 -0700

    CDPD-27519 HIVE-22967 : Support hive.reloadable.aux.jars.path for Hive on Tez ( Toshihiko Uchida via Ashutosh Chauhan)
    Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>
    (cherry picked from commit 48c5ff1dc34582cc1d597b984e9e8122c017c99e)
    Change-Id: I965d050bf1b5d00c9548ce1d79c1269aca76ce58

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/DagUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/DagUtils.java
@@ -1162,7 +1162,9 @@ public class DagUtils {
     String addedFiles = Utilities.getLocalResourceFiles(conf, SessionState.ResourceType.FILE);
     String addedJars = Utilities.getLocalResourceFiles(conf, SessionState.ResourceType.JAR);
     String auxJars = HiveConf.getVar(conf, HiveConf.ConfVars.HIVEAUXJARS);
-    String allFiles = auxJars + "," + addedJars + "," + addedFiles;
+    String reloadableAuxJars = SessionState.get() == null ? null : SessionState.get().getReloadableAuxJars();
+    String allFiles =
+        HiveStringUtils.joinIgnoringEmpty(new String[]{auxJars, reloadableAuxJars, addedJars, addedFiles}, ',');
     return allFiles.split(",");
   }
 
@@ -1179,10 +1181,12 @@ public class DagUtils {
       HiveConf.setVar(conf, ConfVars.HIVEADDEDJARS, addedJars);
     }
     String auxJars = HiveConf.getVar(conf, HiveConf.ConfVars.HIVEAUXJARS);
+    String reloadableAuxJars = SessionState.get() == null ? null : SessionState.get().getReloadableAuxJars();
 
     // need to localize the additional jars and files
     // we need the directory on hdfs to which we shall put all these files
-    String allFiles = auxJars + "," + addedJars + "," + addedFiles;
+    String allFiles =
+        HiveStringUtils.joinIgnoringEmpty(new String[]{auxJars, reloadableAuxJars, addedJars, addedFiles}, ',');
     return allFiles.split(",");
   }
 
