c31e809f1c816638f8b9eca0a910335577845f0b
Karen Coppage <karen.coppage@cloudera.com>
2020-05-27 20:40:42 +0200

    CDPD-12570: HIVE-23492: Remove unnecessary FileSystem#exists calls from ql module (Karen Coppage via Peter Vary) (addendum)             Original backport was partial.
    Change-Id: Ia7ded7e16696a3a0518bc6dcb3385ac37142ab54

--- ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/repl/ReplDumpTask.java
@@ -97,6 +97,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Serializable;
@@ -663,8 +664,10 @@ public class ReplDumpTask extends Task<ReplDumpWork> implements Serializable {
         Path bootstrapRoot = new Path(dumpRoot, ReplUtils.INC_BOOTSTRAP_ROOT_DIR_NAME);
         Path metadataPath = new Path(bootstrapRoot, EximUtil.METADATA_PATH_NAME);
         FileSystem fs = FileSystem.get(metadataPath.toUri(), conf);
-        if (fs.exists(metadataPath)) {
+        try {
           fs.delete(metadataPath, true);
+        } catch (FileNotFoundException e) {
+          // no worries
         }
         Path dbRootMetadata = new Path(metadataPath, dbName);
         Path dbRootData = new Path(bootstrapRoot, EximUtil.DATA_PATH_NAME + File.separator + dbName);
@@ -765,8 +768,10 @@ public class ReplDumpTask extends Task<ReplDumpWork> implements Serializable {
     try {
       retryable.executeCallable((Callable<Void>) () -> {
         FileSystem fs = FileSystem.get(nextEventRoot.toUri(), conf);
-        if (fs.exists(nextEventRoot))  {
+        try {
           fs.delete(nextEventRoot, true);
+        } catch (FileNotFoundException e) {
+          // no worries
         }
         return null;
       });
