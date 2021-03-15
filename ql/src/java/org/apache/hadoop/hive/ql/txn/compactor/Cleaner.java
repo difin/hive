6588f3c2b5abe2b04bdf131b14bff97085e38ebc
Denys Kuzmenko <dkuzmenko@cloudera.com>
2021-03-15 09:01:18 +0100

    CDPD-25519: HIVE-24871: Initiator / Cleaner performance  performance metrics (Denys Kuzmenko, reviewed by Peter Varga)
    Closes (#2061)
    
    (cherry picked from commit cca0da1b393e4cc02c033f78577266f2f7fa7ffd)
    Change-Id: If849dbf29bbc6937f9bc8b72550db72e319162d7

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Cleaner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Cleaner.java
@@ -26,6 +26,10 @@ import org.apache.hadoop.hive.metastore.api.GetValidWriteIdsRequest;
 import org.apache.hadoop.hive.metastore.api.GetValidWriteIdsResponse;
 import org.apache.hadoop.hive.metastore.api.NoSuchObjectException;
 import org.apache.hadoop.hive.metastore.api.NoSuchTxnException;
+import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
+import org.apache.hadoop.hive.metastore.metrics.MetricsConstants;
+import org.apache.hadoop.hive.metastore.metrics.PerfLogger;
+import org.apache.hadoop.hive.metastore.txn.TxnStore;
 import org.apache.hadoop.hive.metastore.txn.TxnUtils;
 import org.apache.hadoop.hive.ql.io.AcidDirectory;
 import org.slf4j.Logger;
@@ -89,6 +93,7 @@ public class Cleaner extends MetaStoreCompactorThread {
   public void run() {
     LOG.info("Starting Cleaner thread");
     try {
+      boolean metricsEnabled = MetastoreConf.getBoolVar(conf, MetastoreConf.ConfVars.METRICS_ENABLED);
       do {
         TxnStore.MutexAPI.LockHandle handle = null;
         long startedAt = -1;
@@ -119,7 +124,7 @@ public class Cleaner extends MetaStoreCompactorThread {
             // and minTxnIdSeenOpen can be removed and minOpenTxnId can be used instead.
             for (CompactionInfo compactionInfo : readyToClean) {
               cleanerList.add(CompletableFuture.runAsync(CompactorUtil.ThrowingRunnable.unchecked(() ->
-                      clean(compactionInfo, cleanerWaterMark)), cleanerExecutor));
+                      clean(compactionInfo, cleanerWaterMark, metricsEnabled)), cleanerExecutor));
             }
             CompletableFuture.allOf(cleanerList.toArray(new CompletableFuture[0])).join();
           }
@@ -146,9 +151,14 @@ public class Cleaner extends MetaStoreCompactorThread {
     }
   }
 
-  private void clean(CompactionInfo ci, long minOpenTxnGLB) throws MetaException {
+  private void clean(CompactionInfo ci, long minOpenTxnGLB, boolean metricsEnabled) throws MetaException {
     LOG.info("Starting cleaning for " + ci);
+    PerfLogger perfLogger = PerfLogger.getPerfLogger(false);
+    String cleanerMetric = MetricsConstants.COMPACTION_CLEANER_CYCLE + "_" + ci.type;
     try {
+      if (metricsEnabled) {
+        perfLogger.PerfLogBegin(CLASS_NAME, cleanerMetric);
+      }
       Table t = resolveTable(ci);
       if (t == null) {
         // The table was dropped before we got around to cleaning it.
@@ -239,6 +249,10 @@ public class Cleaner extends MetaStoreCompactorThread {
           StringUtils.stringifyException(e));
       ci.errorMessage = e.getMessage();
       txnHandler.markFailed(ci);
+    } finally {
+      if (metricsEnabled) {
+        perfLogger.PerfLogEnd(CLASS_NAME, cleanerMetric);
+      }
     }
   }
 
