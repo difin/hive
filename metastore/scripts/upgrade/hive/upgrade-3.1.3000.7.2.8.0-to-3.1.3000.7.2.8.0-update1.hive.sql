fcb3a7172972f4538eb3194c9117ce2dbe40c6f5
Ayush Saxena <ayushsaxena@cloudera.com>
2021-08-26 12:30:53 +0530

    CDPD-26900: HIVE-25301. Expose notification log table through sys db.
    Change-Id: I3c8110a3b2483b82d27b05278c3959620a30efd1

---
+++ metastore/scripts/upgrade/hive/upgrade-3.1.3000.7.2.8.0-to-3.1.3000.7.2.8.0-update1.hive.sql
@@ -0,0 +1,36 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.8.0 to 3.1.3000.7.2.8.0-update1';
+
+USE SYS;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `NOTIFICATION_LOG` (
+  `NL_ID` bigint,
+  `EVENT_ID` bigint,
+  `EVENT_TIME` int,
+  `EVENT_TYPE` varchar(32),
+  `CAT_NAME` varchar(256),
+  `DB_NAME` varchar(128),
+  `TBL_NAME` varchar(256),
+  `MESSAGE` string,
+  `MESSAGE_FORMAT` varchar(16)
+)
+STORED BY 'org.apache.hive.storage.jdbc.JdbcStorageHandler'
+TBLPROPERTIES (
+"hive.sql.database.type" = "METASTORE",
+"hive.sql.query" =
+"SELECT
+    \"NL_ID\",
+    \"EVENT_ID\",
+    \"EVENT_TIME\",
+    \"EVENT_TYPE\",
+    \"CAT_NAME\",
+    \"DB_NAME\",
+    \"TBL_NAME\",
+    \"MESSAGE\",
+    \"MESSAGE_FORMAT\"
+FROM \"NOTIFICATION_LOG\""
+);
+
+CREATE OR REPLACE VIEW SYS.CDH_VERSION AS SELECT 1 AS VER_ID, '3.1.3000.7.2.8.0-update1' AS SCHEMA_VERSION,
+  'Hive release version 3.1.3000 for CDH 7.2.8.0-update1' AS VERSION_COMMENT;
+
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.8.0 to 3.1.3000.7.2.8.0-update1';
