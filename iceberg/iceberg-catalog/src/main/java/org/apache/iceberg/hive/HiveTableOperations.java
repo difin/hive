cee84b95cd7ad4ca032312c4cb197786f5494728
Marton Bod <marton.bod@gmail.com>
2021-08-27 16:02:57 +0200

    CDPD-34717: Backport HIVE-25486: Upgrade iceberg to 0.12.0 (#2603)
    (cherry picked from commit cc289b98fc204d97a29898b46020ddb21b4b9d34)
    
    Change-Id: I8e2d52a7e2dad5be8e68824b84e02985f21c1253

--- iceberg/iceberg-catalog/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
+++ iceberg/iceberg-catalog/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
@@ -508,6 +508,7 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
       return metadata.propertyAsBoolean(TableProperties.ENGINE_HIVE_ENABLED, false);
     }
 
-    return conf.getBoolean(ConfigProperties.ENGINE_HIVE_ENABLED, TableProperties.ENGINE_HIVE_ENABLED_DEFAULT);
+    // As per CDPD-34093, our new default should be true in TableProperties.ENGINE_HIVE_ENABLED_DEFAULT
+    return conf.getBoolean(ConfigProperties.ENGINE_HIVE_ENABLED, true);
   }
 }
