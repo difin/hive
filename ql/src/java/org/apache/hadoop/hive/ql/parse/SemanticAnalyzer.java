6afecd4182af83c6284e19bc9c2962572ecbb7e5
kkasa <kkasa@cloudera.com>
2021-09-10 18:54:53 +0200

    CDPD-26977: HIVE-25512: Merge statement does not enforce check constraints
    Change-Id: I159a0462f2ca071b4b9ceee9a8018f122a77d85c

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7296,7 +7296,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       return input;
     }
 
-    if (updating(dest) && isCBOExecuted()) {
+      if (updating(dest) && isCBOExecuted() && this.ctx.getOperation() != Context.Operation.MERGE) {
       // for UPDATE statements CBO already added and pushed down the constraints
       return input;
     }
