8d021ccba4c0a690b83360a203441a1581e544ae
hmangla98 <haymant.mangla@cloudera.com>
2021-09-30 06:29:21 +0530

    CDPD-30007: HIVE-25534: Error when executing DistCp on file system not supporting XAttrs. (Haymant Mangla, reviewed by Pravin Kumar Sinha
    Change-Id: I825527ec2b812a9f7f997c1d5a932a716bd08841

--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -272,6 +272,16 @@ public class Hadoop23Shims extends HadoopShimsSecure {
     //no-op
   }
 
+  private boolean checkFileSystemXAttrSupport(FileSystem fs) throws IOException {
+    try {
+      fs.getXAttrs(new Path(Path.SEPARATOR));
+      return true;
+    } catch (UnsupportedOperationException e) {
+      LOG.warn("XAttr won't be preserved since it is not supported for file system: " + fs.getUri());
+      return false;
+    }
+  }
+
   /**
    * Returns a shim to wrap MiniMrCluster
    */
@@ -1100,10 +1110,10 @@ public class Hadoop23Shims extends HadoopShimsSecure {
 
   private static final String DISTCP_OPTIONS_PREFIX = "distcp.options.";
 
-  List<String> constructDistCpParams(List<Path> srcPaths, Path dst, Configuration conf) {
+  List<String> constructDistCpParams(List<Path> srcPaths, Path dst, Configuration conf) throws IOException {
     // -update and -delete are mandatory options for directory copy to work.
-    // -pbx is default preserve options if user doesn't pass any.
-    List<String> params = constructDistCpDefaultParams(conf);
+    List<String> params = constructDistCpDefaultParams(conf, dst.getFileSystem(conf),
+            srcPaths.get(0).getFileSystem(conf));
     if (!params.contains("-delete")) {
       params.add("-delete");
     }
@@ -1114,7 +1124,8 @@ public class Hadoop23Shims extends HadoopShimsSecure {
     return params;
   }
 
-  private List<String> constructDistCpDefaultParams(Configuration conf) {
+  private List<String> constructDistCpDefaultParams(Configuration conf, FileSystem dstFs,
+                                                    FileSystem sourceFs) throws IOException {
     List<String> params = new ArrayList<String>();
     boolean needToAddPreserveOption = true;
     for (Map.Entry<String,String> entry : conf.getPropsWithPrefix(DISTCP_OPTIONS_PREFIX).entrySet()){
@@ -1129,7 +1140,7 @@ public class Hadoop23Shims extends HadoopShimsSecure {
       }
     }
     if (needToAddPreserveOption) {
-      params.add("-pbx");
+      params.add((checkFileSystemXAttrSupport(dstFs) && checkFileSystemXAttrSupport(sourceFs)) ? "-pbx" : "-pb");
     }
     if (!params.contains("-update")) {
       params.add("-update");
@@ -1148,9 +1159,10 @@ public class Hadoop23Shims extends HadoopShimsSecure {
    * @return
    */
   List<String> constructDistCpWithSnapshotParams(List<Path> srcPaths, Path dst, String sourceSnap, String destSnap,
-      Configuration conf, String diff) {
+      Configuration conf, String diff) throws IOException {
     // Get the default distcp params
-    List<String> params = constructDistCpDefaultParams(conf);
+    List<String> params = constructDistCpDefaultParams(conf, dst.getFileSystem(conf),
+            srcPaths.get(0).getFileSystem(conf));
     if (params.contains("-delete")) {
       params.remove("-delete");
     }
@@ -1190,18 +1202,11 @@ public class Hadoop23Shims extends HadoopShimsSecure {
 
   @Override
   public boolean runDistCp(List<Path> srcPaths, Path dst, Configuration conf) throws IOException {
-    DistCpOptions options = new DistCpOptions.Builder(srcPaths, dst)
-            .withSyncFolder(true)
-            .withDeleteMissing(true)
-            .preserve(FileAttribute.BLOCKSIZE)
-            .preserve(FileAttribute.XATTR)
-            .build();
-
     // Creates the command-line parameters for distcp
     List<String> params = constructDistCpParams(srcPaths, dst, conf);
     DistCp distcp = null;
     try {
-      distcp = new DistCp(conf, options);
+      distcp = new DistCp(conf, null);
       distcp.getConf().setBoolean("mapred.mapper.new-api", true);
 
       // HIVE-13704 states that we should use run() instead of execute() due to a hadoop known issue
@@ -1228,14 +1233,10 @@ public class Hadoop23Shims extends HadoopShimsSecure {
   public boolean runDistCpWithSnapshots(String oldSnapshot, String newSnapshot, List<Path> srcPaths, Path dst,
       boolean overwriteTarget, Configuration conf)
       throws IOException {
-    DistCpOptions options =
-        new DistCpOptions.Builder(srcPaths, dst).withSyncFolder(true).withUseDiff(oldSnapshot, newSnapshot)
-            .preserve(FileAttribute.BLOCKSIZE).preserve(FileAttribute.XATTR).build();
-
     List<String> params = constructDistCpWithSnapshotParams(srcPaths, dst, oldSnapshot, newSnapshot, conf, "-diff");
     try {
-      conf.setBoolean("mapred.mapper.new-api", true);
-      DistCp distcp = new DistCp(conf, options);
+      DistCp distcp = new DistCp(conf, null);
+      distcp.getConf().setBoolean("mapred.mapper.new-api", true);
       int returnCode = distcp.run(params.toArray(new String[0]));
       if (returnCode == 0) {
         return true;
@@ -1251,7 +1252,7 @@ public class Hadoop23Shims extends HadoopShimsSecure {
           LOG.warn("Copy failed due to target modified. Attempting to restore back the target. source: {} target: {} "
               + "snapshot: {}", srcPaths, dst, oldSnapshot);
           List<String> rParams = constructDistCpWithSnapshotParams(srcPaths, dst, ".", oldSnapshot, conf, "-rdiff");
-          DistCp rDistcp = new DistCp(conf, options);
+          DistCp rDistcp = new DistCp(conf, null);
           returnCode = rDistcp.run(rParams.toArray(new String[0]));
           if (returnCode == 0) {
             LOG.info("Target restored to previous state.  source: {} target: {} snapshot: {}. Reattempting to copy.",
@@ -1271,8 +1272,6 @@ public class Hadoop23Shims extends HadoopShimsSecure {
       }
     } catch (Exception e) {
       throw new IOException("Cannot execute DistCp process: ", e);
-    } finally {
-      conf.setBoolean("mapred.mapper.new-api", false);
     }
     return false;
   }
