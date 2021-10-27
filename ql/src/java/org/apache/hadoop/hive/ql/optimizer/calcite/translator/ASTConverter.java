fb1d7c540ee8c81a355acf87f6103ea5ff0ceaad
scarlin-cloudera <55709772+scarlin-cloudera@users.noreply.github.com>
2021-10-27 10:58:20 -0700

    HIVE-25528: Avoid recalculating types after CBO on second AST pass (#2722) (Stephen Carlin reviewed by Alessandro Solimando, Zoltan Haindrich)
    At compile time, Hive parses the query into ASTNodes.  For CBO, the
    ASTNodes get converted into Calcite RexNodes and then get converted
    back into ASTNodes. After the optimization step is done, the types
    chosen for each step should be the final type. This commit eliminates
    any type changes done on the conversion back to the ASTNode.
    
    A couple of ptest files changed with this commit. The reason for the
    change is because there was an extra constant fold done on the conversion
    back to the ASTNode. This constant fold should have been done at
    optimization time. A Jira will be filed for this, but there is no adverse
    effect for this issue.
    
    Change-Id: If7e4a39254db064f9543a33b003c0013e3e00e95

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/ASTConverter.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/translator/ASTConverter.java
@@ -744,7 +744,7 @@ public class ASTConverter {
           astNodeLst.add(operand.accept(this));
         }
         return SqlFunctionConverter.buildAST(SqlStdOperatorTable.NOT,
-          Collections.singletonList(SqlFunctionConverter.buildAST(SqlStdOperatorTable.IS_NOT_DISTINCT_FROM, astNodeLst)));
+          Collections.singletonList(SqlFunctionConverter.buildAST(SqlStdOperatorTable.IS_NOT_DISTINCT_FROM, astNodeLst, call.getType())), call.getType());
       case CAST:
         assert(call.getOperands().size() == 1);
         if (call.getType().isStruct() ||
@@ -791,7 +791,7 @@ public class ASTConverter {
       if (isFlat(call)) {
         return SqlFunctionConverter.buildAST(op, astNodeLst, 0);
       } else {
-        return SqlFunctionConverter.buildAST(op, astNodeLst);
+        return SqlFunctionConverter.buildAST(op, astNodeLst, call.getType());
       }
     }
   }
