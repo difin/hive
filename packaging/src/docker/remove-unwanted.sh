4f3e9df1989262e8ce5fcb7da7edc8d76be10f1c
Yu-Wen Lai <yu-wen.lai@cloudera.com>
2022-04-12 16:53:56 -0700

    Revert "DWX-10271: Log Div not coming after query execution in Hue in Hive Compute"
    This reverts commit 1561ec365e14939f013496f98bf327e59427b362.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Id517f4b3a4e02672d6b108aab451e0b56431f941

--- packaging/src/docker/remove-unwanted.sh
+++ packaging/src/docker/remove-unwanted.sh
@@ -4,7 +4,6 @@
 find . -name *.tar.gz | xargs rm -f
 find . -name *curator*-2.* | xargs rm -f
 rm -rf /usr/lib/hadoop/share/hadoop/hdfs/webapps/
-rm -rf /usr/lib/hadoop/share/hadoop/hdfs/lib/log4j-*
 rm -rf /usr/lib/hadoop/share/hadoop/common/sources/
 rm -rf /usr/lib/hadoop/share/hadoop/common/webapps/
 rm -rf /usr/lib/hadoop/share/hadoop/mapreduce/sources/
@@ -12,7 +11,6 @@ rm -rf /usr/lib/hadoop/share/hadoop/yarn/timelineservice
 rm -rf /usr/lib/hadoop/share/hadoop/yarn/sources/
 rm -rf /usr/lib/hadoop/share/hadoop/yarn/webapps
 rm -rf /usr/lib/hadoop/share/hadoop/common/lib/jackson-*
-rm -rf /usr/lib/hadoop/share/hadoop/common/lib/log4j-*
 rm -rf /usr/lib/hive/jdbc
 rm -rf /usr/lib/hive/auxlib
 rm -rf /usr/lib/hive/data
@@ -20,7 +18,6 @@ rm -rf /usr/lib/hive/contrib
 rm -rf /usr/lib/hive/docs
 rm -rf /usr/lib/hive/examples
 rm -rf /usr/lib/hive/hcatalog
-rm -rf /usr/lib/hive/log4j-*
 rm -rf /usr/lib/hive/lib/php
 rm -rf /usr/lib/hive/lib/py
 rm -rf /usr/lib/hive/lib/hbase*
