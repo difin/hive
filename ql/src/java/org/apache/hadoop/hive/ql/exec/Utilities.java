difin/difin_hive

HIVE-24679: Reuse FullDPSpecs in loadDynamicPartitions to avoid double listing (#1903) (Peter Varga, reviewed by Marta Kuczora)

ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -76,6 +76,7 @@ import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
+import com.google.common.collect.Maps;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -2847,71 +2848,100 @@ public final class Utilities {
     return function.getTasks();
   }
 
+
+  public static final class PartitionDetails {
+    public Map<String, String> fullSpec;
+    public Partition partition;
+    public List<Path> newFiles;
+    public boolean hasOldPartition = false;
+    public AcidUtils.TableSnapshot tableSnapshot;
+  }
+
   /**
    * Construct a list of full partition spec from Dynamic Partition Context and the directory names
    * corresponding to these dynamic partitions.
    */
-  public static List<LinkedHashMap<String, String>> getFullDPSpecs(Configuration conf, DynamicPartitionCtx dpCtx,
-      long writeId, boolean isMmTable, boolean isDirectInsert, boolean isInsertOverwrite, AcidUtils.Operation acidOperation, Set<String> dynamiPartitionSpecs) throws HiveException {
+  public static Map<Path, PartitionDetails> getFullDPSpecs(Configuration conf, DynamicPartitionCtx dpCtx, long writeId,
+      int stmtId, boolean isMmTable, boolean isDirectInsert, boolean isInsertOverwrite,
+      AcidUtils.Operation acidOperation, Set<String> dynamiPartitionSpecs) throws HiveException {
 
     try {
       Path loadPath = dpCtx.getRootPath();
       FileSystem fs = loadPath.getFileSystem(conf);
       int numDPCols = dpCtx.getNumDPCols();
-      List<Path> path = new ArrayList<>();
+      List<Path> allPartition = new ArrayList<>();
       if (isMmTable || isDirectInsert) {
-        Path[] leafStatus = Utilities.getDirectInsertDirectoryCandidates(fs, loadPath, numDPCols, null, writeId, -1,
+        int stmtIdToUse = -1;
+        if (isDirectInsert) {
+          stmtIdToUse = stmtId;
+        }
+        Path[] leafStatus = Utilities.getDirectInsertDirectoryCandidates(fs, loadPath, numDPCols, null, writeId, stmtIdToUse,
             conf, isInsertOverwrite, acidOperation);
         for (Path p : leafStatus) {
           Path dpPath = p.getParent();
           String partitionSpec = dpPath.toString().substring(loadPath.toString().length() + 1);
-          if (!path.contains(dpPath)) {
+          if (!allPartition.contains(dpPath)) {
             if (isInsertOverwrite) {
               if (dynamiPartitionSpecs == null || dynamiPartitionSpecs.contains(partitionSpec)) {
-                path.add(dpPath);
+                allPartition.add(dpPath);
               }
             }
             else {
-              path.add(dpPath);
+              allPartition.add(dpPath);
             }
           }
         }
       } else {
         List<FileStatus> status = HiveStatsUtils.getFileStatusRecurse(loadPath, numDPCols, fs);
         for (FileStatus fileStatus : status) {
-          path.add(fileStatus.getPath());
+          allPartition.add(fileStatus.getPath());
         }
       }
 
-      if (path.isEmpty()) {
+      if (allPartition.isEmpty()) {
         LOG.warn("No partition is generated by dynamic partitioning");
-        return null;
+        return Collections.synchronizedMap(new LinkedHashMap<>());
       }
+      validateDynPartitionCount(conf, allPartition);
 
       // partial partition specification
       Map<String, String> partSpec = dpCtx.getPartSpec();
 
       // list of full partition specification
-      List<LinkedHashMap<String, String>> fullPartSpecs = new ArrayList<LinkedHashMap<String, String>>();
+      Map<Path, PartitionDetails> partitionDetailsMap =
+          Collections.synchronizedMap(new LinkedHashMap<>());
 
-      // for each dynamically created DP directory, construct a full partition spec
-      // and load the partition based on that
-      for (Path partPath : path) {
-        assert fs.getFileStatus(partPath).isDir() : "partitions " + partPath
-            + " is not a directory !";
-
-        // generate a full partition specification
-        LinkedHashMap<String, String> fullPartSpec = new LinkedHashMap<String, String>(partSpec);
-        if (!Warehouse.makeSpecFromName(fullPartSpec, partPath, new HashSet<String>(partSpec.keySet()))) {
-          Utilities.FILE_OP_LOGGER.warn("Ignoring invalid DP directory {}", partPath);
-          continue;
+      // calculate full path spec for each valid partition path
+      allPartition.forEach(partPath -> {
+        Map<String, String> fullPartSpec = Maps.newLinkedHashMap(partSpec);
+        String staticParts =  Warehouse.makeDynamicPartName(partSpec);
+        Path computedPath = partPath;
+        if (!staticParts.isEmpty() ) {
+          computedPath = new Path(new Path(partPath.getParent(), staticParts), partPath.getName());
         }
-        Utilities.FILE_OP_LOGGER.trace("Adding partition spec from {}: {}", partPath, fullPartSpec);
-        fullPartSpecs.add(fullPartSpec);
-      }
-      return fullPartSpecs;
+        if (!Warehouse.makeSpecFromName(fullPartSpec, computedPath, new HashSet<>(partSpec.keySet()))) {
+          Utilities.FILE_OP_LOGGER.warn("Ignoring invalid DP directory " + partPath);
+        } else {
+          PartitionDetails details = new PartitionDetails();
+          details.fullSpec = fullPartSpec;
+          partitionDetailsMap.put(partPath, details);
+        }
+      });
+      return partitionDetailsMap;
     } catch (IOException e) {
       throw new HiveException(e);
+    }
+  }
+
+  private static void validateDynPartitionCount(Configuration conf, List<Path> partitions) throws HiveException {
+    int partsToLoad = partitions.size();
+    int maxPartition = HiveConf.getIntVar(conf, HiveConf.ConfVars.DYNAMICPARTITIONMAXPARTS);
+    if (partsToLoad > maxPartition) {
+      throw new HiveException("Number of dynamic partitions created is " + partsToLoad
+          + ", which is more than "
+          + maxPartition
+          +". To solve this try to set " + HiveConf.ConfVars.DYNAMICPARTITIONMAXPARTS.varname
+          + " to at least " + partsToLoad + '.');
     }
   }
 

difin/hive

(cherry picked from commit 25c43b8044f49982926f6b9e01307fe055f2fe06)
Change-Id: Icc6c9d435002902c913bc756963250d2e12f45ab

ql/src/java/org/apache/hadoop/hive/ql/exec/Utilities.java
@@ -79,6 +79,7 @@ import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
+import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import org.apache.commons.codec.binary.Base64;
@@ -2965,71 +2966,100 @@ public final class Utilities {
     return function.getTasks();
   }
 
+
+  public static final class PartitionDetails {
+    public Map<String, String> fullSpec;
+    public Partition partition;
+    public List<FileStatus> newFiles;
+    public boolean hasOldPartition = false;
+    public AcidUtils.TableSnapshot tableSnapshot;
+  }
+
   /**
    * Construct a list of full partition spec from Dynamic Partition Context and the directory names
    * corresponding to these dynamic partitions.
    */
-  public static List<LinkedHashMap<String, String>> getFullDPSpecs(Configuration conf, DynamicPartitionCtx dpCtx,
-      long writeId, boolean isMmTable, boolean isDirectInsert, boolean isInsertOverwrite, AcidUtils.Operation acidOperation, Set<String> dynamiPartitionSpecs) throws HiveException {
+  public static Map<Path, PartitionDetails> getFullDPSpecs(Configuration conf, DynamicPartitionCtx dpCtx, long writeId,
+      int stmtId, boolean isMmTable, boolean isDirectInsert, boolean isInsertOverwrite,
+      AcidUtils.Operation acidOperation, Set<String> dynamiPartitionSpecs) throws HiveException {
 
     try {
       Path loadPath = dpCtx.getRootPath();
       FileSystem fs = loadPath.getFileSystem(conf);
       int numDPCols = dpCtx.getNumDPCols();
-      List<Path> path = new ArrayList<>();
+      List<Path> allPartition = new ArrayList<>();
       if (isMmTable || isDirectInsert) {
-        Path[] leafStatus = Utilities.getDirectInsertDirectoryCandidates(fs, loadPath, numDPCols, null, writeId, -1,
+        int stmtIdToUse = -1;
+        if (isDirectInsert) {
+          stmtIdToUse = stmtId;
+        }
+        Path[] leafStatus = Utilities.getDirectInsertDirectoryCandidates(fs, loadPath, numDPCols, null, writeId, stmtIdToUse,
             conf, isInsertOverwrite, acidOperation);
         for (Path p : leafStatus) {
           Path dpPath = p.getParent();
           String partitionSpec = dpPath.toString().substring(loadPath.toString().length() + 1);
-          if (!path.contains(dpPath)) {
+          if (!allPartition.contains(dpPath)) {
             if (isInsertOverwrite) {
               if (dynamiPartitionSpecs == null || dynamiPartitionSpecs.contains(partitionSpec)) {
-                path.add(dpPath);
+                allPartition.add(dpPath);
               }
             }
             else {
-              path.add(dpPath);
+              allPartition.add(dpPath);
             }
           }
         }
       } else {
         List<FileStatus> status = HiveStatsUtils.getFileStatusRecurse(loadPath, numDPCols, fs);
         for (FileStatus fileStatus : status) {
-          path.add(fileStatus.getPath());
+          allPartition.add(fileStatus.getPath());
         }
       }
 
-      if (path.isEmpty()) {
+      if (allPartition.isEmpty()) {
         LOG.warn("No partition is generated by dynamic partitioning");
-        return null;
+        return Collections.synchronizedMap(new LinkedHashMap<>());
       }
+      validateDynPartitionCount(conf, allPartition);
 
       // partial partition specification
       Map<String, String> partSpec = dpCtx.getPartSpec();
 
       // list of full partition specification
-      List<LinkedHashMap<String, String>> fullPartSpecs = new ArrayList<LinkedHashMap<String, String>>();
+      Map<Path, PartitionDetails> partitionDetailsMap =
+          Collections.synchronizedMap(new LinkedHashMap<>());
 
-      // for each dynamically created DP directory, construct a full partition spec
-      // and load the partition based on that
-      for (Path partPath : path) {
-        assert fs.getFileStatus(partPath).isDir() : "partitions " + partPath
-            + " is not a directory !";
-
-        // generate a full partition specification
-        LinkedHashMap<String, String> fullPartSpec = new LinkedHashMap<String, String>(partSpec);
-        if (!Warehouse.makeSpecFromName(fullPartSpec, partPath, new HashSet<String>(partSpec.keySet()))) {
-          Utilities.FILE_OP_LOGGER.warn("Ignoring invalid DP directory {}", partPath);
-          continue;
+      // calculate full path spec for each valid partition path
+      allPartition.forEach(partPath -> {
+        Map<String, String> fullPartSpec = Maps.newLinkedHashMap(partSpec);
+        String staticParts =  Warehouse.makeDynamicPartName(partSpec);
+        Path computedPath = partPath;
+        if (!staticParts.isEmpty() ) {
+          computedPath = new Path(new Path(partPath.getParent(), staticParts), partPath.getName());
         }
-        Utilities.FILE_OP_LOGGER.trace("Adding partition spec from {}: {}", partPath, fullPartSpec);
-        fullPartSpecs.add(fullPartSpec);
-      }
-      return fullPartSpecs;
+        if (!Warehouse.makeSpecFromName(fullPartSpec, computedPath, new HashSet<>(partSpec.keySet()))) {
+          Utilities.FILE_OP_LOGGER.warn("Ignoring invalid DP directory " + partPath);
+        } else {
+          PartitionDetails details = new PartitionDetails();
+          details.fullSpec = fullPartSpec;
+          partitionDetailsMap.put(partPath, details);
+        }
+      });
+      return partitionDetailsMap;
     } catch (IOException e) {
       throw new HiveException(e);
+    }
+  }
+
+  private static void validateDynPartitionCount(Configuration conf, List<Path> partitions) throws HiveException {
+    int partsToLoad = partitions.size();
+    int maxPartition = HiveConf.getIntVar(conf, HiveConf.ConfVars.DYNAMICPARTITIONMAXPARTS);
+    if (partsToLoad > maxPartition) {
+      throw new HiveException("Number of dynamic partitions created is " + partsToLoad
+          + ", which is more than "
+          + maxPartition
+          +". To solve this try to set " + HiveConf.ConfVars.DYNAMICPARTITIONMAXPARTS.varname
+          + " to at least " + partsToLoad + '.');
     }
   }
 