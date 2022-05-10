ef25d29968a3fda14d58f2a4a79283618851e015
Ramesh Thangarajan <rameshkumar@cloudera.com>
2022-05-10 16:27:08 -0700

    HIVE-26219 Encapsulate the API change for FileUtils.isActionPermittedForFileHierarchy
    Change-Id: Ic4404ebada2892716efbb12c7c0f6c1131797a51

--- common/src/java/org/apache/hadoop/hive/common/FileUtils.java
+++ common/src/java/org/apache/hadoop/hive/common/FileUtils.java
@@ -515,6 +515,20 @@ public final class FileUtils {
   }
 
   public static boolean isActionPermittedForFileHierarchy(FileSystem fs, FileStatus fileStatus,
+      String userName, FsAction action, boolean recurse) throws Exception {
+    UserGroupInformation proxyUser = null;
+    boolean isPermitted;
+    try {
+      proxyUser = getProxyUser(userName);
+      FileSystem fsAsUser = getFsAsUser(fs, proxyUser);
+      isPermitted = isActionPermittedForFileHierarchy(fs, fileStatus, userName, action, recurse, fsAsUser);
+    } finally {
+      closeFs(proxyUser);
+    }
+    return isPermitted;
+  }  
+
+  public static boolean isActionPermittedForFileHierarchy(FileSystem fs, FileStatus fileStatus,
       String userName, FsAction action, boolean recurse, FileSystem fsAsUser) throws Exception {
     boolean isDir = fileStatus.isDir();
 
