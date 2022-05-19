86dc00c36acb5381f60f606b4b4eddea2830c5c4
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2022-05-19 11:35:59 +0200

    DWX-12529: Hive: implement a helper script for downloading dependency tars
    Change-Id: Iaf6c368dd46df6581baead6cdf73baf577bde220

---
+++ packaging/src/docker/download_dep_tars.sh
@@ -0,0 +1,95 @@
+# This script helps to download dependency tars needed for hive image (referenced in Dockerfile)
+# 1. Fetches latest build for a certain CDH_PREFIX, CDWH_PREFIX
+# 2. If CDH_VERSION, CDWH_VERSION are defined, they're used instead of the fetched build number
+# 3. Resolves the url on which the tar artifacts of the builds are available
+# 4. Downloads artifacts to scripts's folder
+# 5. Prints a recommended way to build hive image from hive repo root folder
+
+PLATFORM="${PLATFORM:-redhat7}"
+CDH_PREFIX="7.2.15.0"       #TODO: get last from https://release.infra.cloudera.com/hwre-api/releasedbuilds?stack=CDH&type=public
+CDWH_PREFIX="2022.0.9.0"
+
+if [ -n "$ZSH_VERSION" ]; then
+   SCRIPT_DIR="${0:a:h}"
+else
+   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+DOCKER_FENG_DIR="$SCRIPT_DIR/../docker-feng"
+CDWH_YEAR="${CDWH_PREFIX%%\.*}"
+
+export CDH_REPO_DETAILS_URL="http://release.infra.cloudera.com/hwre-api/versioninfo?stack=CDH&stack_version=${CDH_PREFIX}&per_page=1"
+if [ -z $CDH_VERSION ]; then #CDH_VERSION can be forced from ENV
+    CDH_VERSION=$(curl -s $CDH_REPO_DETAILS_URL | jq 'keys[0]' | tr -d '"')
+fi
+echo "CDH_VERSION=${CDH_VERSION}"
+CDH_REPO_URL=$(curl -s $CDH_REPO_DETAILS_URL | jq '.["'${CDH_VERSION}'"]'.platforms.${PLATFORM}.repo_url | tr -d '"')
+CDH_GBN=$(curl -s $CDH_REPO_DETAILS_URL | jq '.["'${CDH_VERSION}'"]'.gbn | tr -d '"')
+CDH_TARS_URL=http://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/${CDH_GBN}/cdh/7.x/$PLATFORM/yum/tars/
+echo "CDH_TARS_URL=${CDH_TARS_URL}"
+
+CDWH_BUILD_URL="http://release.infra.cloudera.com/hwre-api/stackinfo?stack=CDWH&build_type=dev"
+if [ -z $CDWH_VERSION ]; then #CDWH_VERSION can be forced from ENV, like: export CDWH_VERSION=2022.0.9.0-14
+    CDWH_VERSION=$(curl -s ${CDWH_BUILD_URL} | jq '.["'${CDWH_PREFIX}'"].last_sucessful_build' | tr -d '"')
+fi
+echo "CDWH_VERSION=${CDWH_VERSION}"
+
+export CDWH_REPO_DETAILS_URL="http://release.infra.cloudera.com/hwre-api/versioninfo?stack=CDWH&stack_version=${CDWH_PREFIX}&per_page=50"
+CDWH_REPO_URL=$(curl -s $CDWH_REPO_DETAILS_URL | jq '.["'${CDWH_VERSION}'"]'.platforms.${PLATFORM}.repo_url | tr -d '"')
+
+CDWH_GBN=$(curl -s $CDWH_REPO_DETAILS_URL | jq '.["'${CDWH_VERSION}'"]'.gbn | tr -d '"')
+CDWH_TARS_URL=http://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/${CDWH_GBN}/cdwh/${CDWH_YEAR}.x/$PLATFORM/yum/tars/
+echo "CDWH_TARS_URL=${CDWH_TARS_URL}"
+
+TEZ_VERSION=0.9.1.$CDWH_VERSION
+HADOOP_VERSION=3.1.1.$CDH_VERSION
+SOLR_VERSION=8.4.1.$CDH_VERSION
+RANGER_VERSION=2.1.0.$CDH_VERSION
+ATLAS_VERSION=2.1.0.$CDH_VERSION
+IMPALA_VERSION=4.0.0.$CDWH_VERSION
+
+HADOOP_TAR_FILE=hadoop-$HADOOP_VERSION.tar.gz
+HADOOP_TAR_URL="$CDH_TARS_URL/hadoop/$HADOOP_TAR_FILE"
+if [ ! -f "$SCRIPT_DIR/$HADOOP_TAR_FILE" ]; then
+    echo "HADOOP_TAR_URL=${HADOOP_TAR_URL}"
+    curl ${HADOOP_TAR_URL} --output $SCRIPT_DIR/$HADOOP_TAR_FILE
+fi
+
+RANGER_TAR_FILE=ranger-$RANGER_VERSION-hive-plugin.tar.gz
+RANGER_TAR_URL="$CDH_TARS_URL/ranger/$RANGER_TAR_FILE"
+if [ ! -f "$SCRIPT_DIR/$RANGER_TAR_FILE" ]; then
+    echo "RANGER_TAR_URL=${RANGER_TAR_URL}"
+    curl ${RANGER_TAR_URL} --output $SCRIPT_DIR/$RANGER_TAR_FILE
+fi
+
+SOLR_TAR_FILE=solr-$SOLR_VERSION.tar.gz
+SOLR_TAR_URL="$CDH_TARS_URL/solr/$SOLR_TAR_FILE"
+if [ ! -f "$SCRIPT_DIR/$SOLR_TAR_FILE" ]; then
+    echo "SOLR_TAR_URL=${SOLR_TAR_URL}"
+    curl ${SOLR_TAR_URL} --output $SCRIPT_DIR/$SOLR_TAR_FILE
+fi
+
+ATLAS_TAR_FILE=apache-atlas-$ATLAS_VERSION-hive-hook.tar.gz
+ATLAS_TAR_URL="$CDH_TARS_URL/atlas/$ATLAS_TAR_FILE"
+if [ ! -f "$SCRIPT_DIR/$ATLAS_TAR_FILE" ]; then
+    echo "ATLAS_TAR_URL=${ATLAS_TAR_URL}"
+    curl ${ATLAS_TAR_URL} --output $SCRIPT_DIR/$ATLAS_TAR_FILE
+fi
+
+TEZ_TAR_FILE=tez-$TEZ_VERSION.tar.gz
+TEZ_TAR_URL="$CDWH_TARS_URL/tez/$TEZ_TAR_FILE"
+if [ ! -f "$SCRIPT_DIR/$TEZ_TAR_FILE" ]; then
+    echo "TEZ_TAR_URL=${TEZ_TAR_URL}"
+    curl ${TEZ_TAR_URL} --output $SCRIPT_DIR/$TEZ_TAR_FILE
+fi
+
+IMPALA_TAR_FILE=impala-fesupport-${IMPALA_VERSION}.tar.gz
+IMPALA_TAR_URL="$CDWH_TARS_URL/impala/$IMPALA_TAR_FILE"
+if [ ! -f "$DOCKER_FENG_DIR/$IMPALA_TAR_FILE" ]; then
+    echo "IMPALA_TAR_URL=${IMPALA_TAR_URL}"
+    curl ${IMPALA_TAR_URL} --output $DOCKER_FENG_DIR/$IMPALA_TAR_FILE
+fi
+
+
+echo "\n*** YOU CAN BUILD HIVE IMAGE AS DESCRIBED BELOW ***\n"
+echo "unset CDH_VERSION # it should be picked up from packaging/pom.xml as the project.version"
+echo "mvn docker:build -Pdocker -pl packaging -DskipTests -Dmaven.javadoc.skip=true -Denforcer.skip=true -Dtez.version=$TEZ_VERSION -Dhadoop.version=$HADOOP_VERSION -Dsolr.version=$SOLR_VERSION -Dranger.version=$RANGER_VERSION -Datlas.version=$ATLAS_VERSION -Dimpala.version=$IMPALA_VERSION -Ddocker.verbose"
