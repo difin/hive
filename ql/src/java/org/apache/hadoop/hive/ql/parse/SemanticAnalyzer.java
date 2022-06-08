d3b41e4db46eaa6ba29a2a4969b088fe96976988
Sourabh Badhya <sbadhya@cloudera.com>
2022-06-08 13:29:20 +0530

    CDPD-41443: HIVE-26266: Column information is missing from lineage for CTAS when custom/translated location is used (Sourabh Badhya, reviewed by  Zoltan Haindrich)
    Closes #3331
    
    (cherry picked from commit c9e12d84ca0c72732cd24aa0b160f474309f5de9)
    Change-Id: I87eb01ca0ec5d61fe711673f33c469ff744f4e36

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7967,7 +7967,8 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       boolean isDfsDir = (destType == QBMetaData.DEST_DFS_FILE);
 
       try {
-        destinationTable = tblDesc != null ? tblDesc.toTable(conf) : viewDesc != null ? viewDesc.toTable(conf) : null;
+        destinationTable = tblDesc != null ? db.getTranslateTableDryrun(tblDesc.toTable(conf).getTTable()) :
+                viewDesc != null ? viewDesc.toTable(conf) : null;
       } catch (HiveException e) {
         throw new SemanticException(e);
       }
@@ -8530,6 +8531,11 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         Warehouse wh = new Warehouse(conf);
         tlocation = wh.getDefaultTablePath(db.getDatabase(tableDesc.getDatabaseName()),
             tName + suffix, tableDesc.isExternal());
+
+        if (destinationTable != null && destinationTable.getSd() != null
+                && destinationTable.getPath() != null) {
+          tlocation = destinationTable.getPath();
+        }
       } catch (MetaException|HiveException e) {
         throw new SemanticException(e);
       }
