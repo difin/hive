5c21bfa98dba4ae8f39ece48db208ee7e5312bce
Abhay Chennagiri <achennagiri@cloudera.com>
2022-07-25 11:06:29 -0700

    CDPD-43047 HIVE-26426: Avoid IOBE in canCBOHandleAst() method. This issue was resulting in failures in custom catalog sys init job.
    Change-Id: Icb9db38559be7af74ae3c93f5f59e99304d72a3f

--- ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/CalcitePlanner.java
@@ -993,7 +993,7 @@ public class CalcitePlanner extends SemanticAnalyzer {
     if (msg == null) {
       return Pair.of(true, msg);
     }
-    msg = msg.substring(0, msg.length() - 2);
+
     if (needToLogMessage) {
       STATIC_LOG.info("Not invoking CBO because the statement " + msg);
     }
@@ -1049,10 +1049,10 @@ public class CalcitePlanner extends SemanticAnalyzer {
       if (queryProperties.hasLateralViews()) {
         msg += "has lateral views; ";
       }
-
       if (msg.isEmpty()) {
         msg += "has some unspecified limitations; ";
       }
+      msg = msg.substring(0, msg.length() - 2);
     }
     return msg;
   }
