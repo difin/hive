6f215b081bd448f9e8cadf16c5985871d02f50b7
Adesh Kumar Rao <adrao@microsoft.com>
2022-08-01 16:45:44 +0530

    CDPD-65202 HIVE-26412: Create interface to fetch available slots and add the default implementation (Adesh Rao, reviewed by Laszlo Bodor, Nikhil Gupta, Sankar Hariappan))
    Signed-off-by: Sankar Hariappan <sankarh@apache.org>
    Closes (#3456)
    Change-Id: Iaf4e6f1c6d23cc0742aa16c523c82263c96038a5

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/HiveSplitGenerator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/HiveSplitGenerator.java
@@ -40,6 +40,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.io.HiveInputFormat;
 import org.apache.hadoop.hive.ql.plan.PartitionDesc;
+import org.apache.hive.common.util.ReflectionUtil;
 import org.apache.tez.common.counters.TezCounters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -187,20 +188,7 @@ public class HiveSplitGenerator extends InputInitializer {
           (InputFormat<?, ?>) ReflectionUtils.newInstance(JavaUtils.loadClass(realInputFormatName),
             jobConf);
 
-        int totalResource = 0;
-        int taskResource = 0;
-        int availableSlots = 0;
-        // FIXME. Do the right thing Luke.
-        if (getContext() == null) {
-          // for now, totalResource = taskResource for llap
-          availableSlots = 1;
-        }
-
-        if (getContext() != null) {
-          totalResource = getContext().getTotalAvailableResource().getMemory();
-          taskResource = getContext().getVertexTaskResource().getMemory();
-          availableSlots = totalResource / taskResource;
-        }
+        int availableSlots = getAvailableSlotsCalculator().getAvailableSlots();
 
         final int configAvailableSlots = HiveConf.getIntVar(conf,
           HiveConf.ConfVars.HIVE_SERVER2_USE_PER_QUERY_LLAP_TOTAL_AVAILABLE_SLOTS);
@@ -454,5 +442,13 @@ public class HiveSplitGenerator extends InputInitializer {
         throw new RuntimeException("Problem getting input split size", e);
       }
     }
+  }
+
+  private AvailableSlotsCalculator getAvailableSlotsCalculator() throws Exception {
+    Class<?> clazz = Class.forName(HiveConf.getVar(conf, HiveConf.ConfVars.HIVE_SPLITS_AVAILABLE_SLOTS_CALCULATOR_CLASS),
+            true, Utilities.getSessionSpecifiedClassLoader());
+    AvailableSlotsCalculator slotsCalculator = (AvailableSlotsCalculator) ReflectionUtil.newInstance(clazz, null);
+    slotsCalculator.initialize(conf, this);
+    return slotsCalculator;
   }
 }
