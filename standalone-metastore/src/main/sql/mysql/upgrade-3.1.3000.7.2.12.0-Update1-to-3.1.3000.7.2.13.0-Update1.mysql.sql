cf03871afd0117ac9a01753e889a27c698419778
Naveen Gangam <ngangam@cloudera.com>
2022-10-12 01:16:49 -0400

    CDPD-44672: Intermediate column size is too high for utf-8 in MySQL (Naveen Gangam)
    Change-Id: I5950b2f2df9903ae269e4ef208a88b8fd4b3bf1a

--- standalone-metastore/src/main/sql/mysql/upgrade-3.1.3000.7.2.12.0-Update1-to-3.1.3000.7.2.13.0-Update1.mysql.sql
+++ standalone-metastore/src/main/sql/mysql/upgrade-3.1.3000.7.2.12.0-Update1-to-3.1.3000.7.2.13.0-Update1.mysql.sql
@@ -1,5 +1,5 @@
 --Increase the size of RM_PROGRESS to accomodate the replication statistics
-ALTER TABLE REPLICATION_METRICS MODIFY RM_PROGRESS varchar(24000);
+ALTER TABLE REPLICATION_METRICS MODIFY RM_PROGRESS varchar(10000);
 
 -- These lines need to be last.  Insert any changes above.
 UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.2.13.0-Update1', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.2.13.0-Update1' where VER_ID=1;
