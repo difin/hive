a7dd24833a235f2e0dd43cfc1aa3d3c7efa5aeb5
Denys Kuzmenko <deniskuzZ@gmail.com>
2022-12-12 10:23:00 +0200

    DWX-13906: HIVE-26811: SharedWorkOptimizer: take the union of virtual columns in mergeable TableScans (Denys Kuzmenko, reviewed by Krisztian Kasa)
    Closes #3837
    
    (cherry picked from commit 6a8e20a5836519e33e2eb7b7f71a250f09874eeb)
    Change-Id: Ibd02351387222becb7990adb7e68721bd38082cc

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/SharedWorkOptimizer.java
@@ -53,6 +53,7 @@ import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.exec.UnionOperator;
 import org.apache.hadoop.hive.ql.metadata.Table;
+import org.apache.hadoop.hive.ql.metadata.VirtualColumn;
 import org.apache.hadoop.hive.ql.parse.GenTezUtils;
 import org.apache.hadoop.hive.ql.parse.ParseContext;
 import org.apache.hadoop.hive.ql.parse.PrunedPartitionList;
@@ -208,7 +209,7 @@ public class SharedWorkOptimizer extends Transform {
       }
     }
 
-    if(pctx.getConf().getBoolVar(ConfVars.HIVE_SHARED_WORK_REUSE_MAPJOIN_CACHE)) {
+    if (pctx.getConf().getBoolVar(ConfVars.HIVE_SHARED_WORK_REUSE_MAPJOIN_CACHE)) {
       // Try to reuse cache for broadcast side in mapjoin operators that
       // share same input.
       // First we group together all the mapjoin operators that share same
@@ -590,6 +591,11 @@ public class SharedWorkOptimizer extends Transform {
       for (String col : discardableTsOp.getConf().getNeededColumns()) {
         if (!retainableTsOp.getConf().getNeededColumns().contains(col)) {
           retainableTsOp.getConf().getNeededColumns().add(col);
+        }
+      }
+      for (VirtualColumn col : discardableTsOp.getConf().getVirtualCols()) {
+        if (!retainableTsOp.getConf().getVirtualCols().contains(col)) {
+          retainableTsOp.getConf().getVirtualCols().add(col);
         }
       }
     }
