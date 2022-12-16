5eac6da9b94f0a6f8fe66f4d767672bf10edf609
Norbert Luksa <norbert.luksa@cloudera.com>
2022-12-16 13:48:51 +0100

    DWX-14014: Remove hardcoded port and service from metastore health check
    While ideally we should get a fix for CDPD-5246, this commit fixes the workaround which hardcodes the postgres port. This is needed since in private cloud the postrges DB can be external with a custom port.
    
    During fixing, I noticed that the host is also wrong, in default DBC we should connect to `postgres-service-default-warehouse` since that is the DB actually used by HMS.
    
    The change should be backward compatible since default values are set to the ones currently used.
    
    Change-Id: I05ecc3c788c902c2eb028eac115ec5c0980e4246

--- packaging/src/docker/healthz-hms.sh
+++ packaging/src/docker/healthz-hms.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Copyright (c) 2020 Cloudera, Inc. All rights reserved.
+# Copyright (c) 2023 Cloudera, Inc. All rights reserved.
 set -e
 set -x
+
+DB_ENDPOINT=${DB_ENDPOINT:-postgres-service}
+DB_PORT=${DB_PORT:-5432}
 
 # FIXME: This is workaround for DWX-1645
 # Proper fix for this will come from CDPD-5246
 
-# This makes sure we can connect to postgres service and query HMS specific metrics exposed as for prometheus via JMX exporter.
+# This makes sure we can connect to database service and query HMS specific metrics exposed as for prometheus via JMX exporter.
 # If HMS is alive the metrics should exist and grep should return 0 exit code.
-nc -zv -w 2 postgres-service 5432 && curl -s http://localhost:35000 | grep "^api_init"
+nc -zv -w 2 $DB_ENDPOINT $DB_PORT && curl -s http://localhost:35000 | grep "^api_init"
