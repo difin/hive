90b4345a4d2af7af8eb6c430c909aa4365ce39f0
Norbert Luksa <norbert.luksa@cloudera.com>
2023-01-02 14:07:58 +0100

    DWX-14075: Add missing FROM to Oracle DB upgrade
    We run into this issue in PvC where running the Oracle upgrade script `upgrade-3.1.3000.7.2.16.0-Update1-to-3.1.3000.7.2.16.0-Update2.oracle.sql` failes due to a missing `FROM` statement. Turns out `FROM dual` is missing from multiple places.
    
    Also changed MESSAGE to Status for consistency, as based on the previous few migrations I checked, this was used.
    
    Change-Id: I336a15d39ceb8e104a2e27e64ab78f02bdf26994

--- standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.16.0-Update1-to-3.1.3000.7.2.16.0-Update2.oracle.sql
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.16.0-Update1-to-3.1.3000.7.2.16.0-Update2.oracle.sql
@@ -1,12 +1,10 @@
-SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update2 to 7.2.16.0-Update2' AS MESSAGE;
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update1 to 3.1.3000.7.2.16.0-Update2' AS Status FROM dual;
 
 -- HIVE-26280
 ALTER TABLE COMPLETED_COMPACTIONS ADD CC_NEXT_TXN_ID NUMBER(19);
 ALTER TABLE COMPLETED_COMPACTIONS ADD CC_TXN_ID NUMBER(19);
 ALTER TABLE COMPLETED_COMPACTIONS ADD CC_COMMIT_TIME NUMBER(19);
 
-
-
 -- These lines need to be last.  Insert any changes above.
 UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.2.16.0-Update2', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.2.16.0-Update2' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update1 to 3.1.3000.7.2.16.0-Update2' AS MESSAGE;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update1 to 3.1.3000.7.2.16.0-Update2' AS Status FROM dual;
--- standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.16.0-Update2-to-3.1.3000.7.2.16.0-Update3.oracle.sql
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.16.0-Update2-to-3.1.3000.7.2.16.0-Update3.oracle.sql
@@ -1,4 +1,4 @@
-SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update2 to 7.2.16.0-Update3' AS MESSAGE;
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update2 to 3.1.3000.7.2.16.0-Update3' AS Status FROM dual;
 
 -- HIVE-26443
 ALTER TABLE COMPACTION_QUEUE ADD CQ_POOL_NAME VARCHAR(128);
@@ -6,4 +6,4 @@ ALTER TABLE COMPLETED_COMPACTIONS ADD CC_POOL_NAME VARCHAR(128);
 
 -- These lines need to be last.  Insert any changes above.
 UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.2.16.0-Update3', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.2.16.0-Update3' where VER_ID=1;
-SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update2 to 3.1.3000.7.2.16.0-Update3' AS MESSAGE;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.16.0-Update2 to 3.1.3000.7.2.16.0-Update3' AS Status FROM dual;
