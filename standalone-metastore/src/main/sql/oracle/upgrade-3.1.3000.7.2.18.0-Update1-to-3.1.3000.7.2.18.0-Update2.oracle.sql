d7ff54e03b5ac08a17f54c0a26a75e29ca277135
Hongdan Zhu <danielzhu@cloudera.com>
2023-02-02 16:33:24 -0800

    CDPD-49032: HIVE-27165: PART_COL_STATS metastore query not hitting the index, change the index order in table TAB_COL_STATS and PART_COL_STATS
    Change-Id: Iaca56a38e173f7f7bb9fcef97c1008cc210c1501

---
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.18.0-Update1-to-3.1.3000.7.2.18.0-Update2.oracle.sql
@@ -0,0 +1,11 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.18.0-Update1 to 3.1.3000.7.2.18.0-Update2' AS Status from dual;
+
+--CDPD-49032
+DROP INDEX TAB_COL_STATS_IDX;
+CREATE INDEX TAB_COL_STATS_IDX ON TAB_COL_STATS (DB_NAME, TABLE_NAME, COLUMN_NAME, CAT_NAME);
+DROP INDEX PCS_STATS_IDX;
+CREATE INDEX PCS_STATS_IDX ON PART_COL_STATS (DB_NAME,TABLE_NAME,COLUMN_NAME,PARTITION_NAME,CAT_NAME);
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.2.18.0-Update2', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.2.18.0-Update2' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.18.0-Update1 to 3.1.3000.7.2.18.0-Update2' AS Status from dual;
