2aa7c2918ac0bba7976ca8a6b9ba3dab55a86416
zchovan <zchovan@cloudera.com>
2023-07-17 19:57:37 +0200

    CDPD-57214: HIVE-27457: Update Kudu table storage handlers in backend db
    Change-Id: Ic0e2465347896614add0155e6b3dad2b55efe050
    (cherry picked from commit 174a088a5facbcd159394813e628a4b3e0643014)

---
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.2.17.0-Update3-to-3.1.3000.7.2.18.0-Update1.oracle.sql
@@ -0,0 +1,9 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.17.0-Update3-to-3.1.3000.7.2.18.0-Update1' AS Status from dual;
+
+-- HIVE-26221
+ALTER TABLE TAB_COL_STATS ADD HISTOGRAM BLOB;
+ALTER TABLE PART_COL_STATS ADD HISTOGRAM BLOB;
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.2.18.0-Update1', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.2.18.0-Update1' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.17.0-Update3-to-3.1.3000.7.2.18.0-Update1' AS Status from dual;
--- standalone-metastore/src/main/sql/postgres/upgrade-3.1.3000.7.2.17.0-Update2-to-3.1.3000.7.2.17.0-Update3.postgres.sql
+++ standalone-metastore/src/main/sql/postgres/upgrade-3.1.3000.7.2.17.0-Update2-to-3.1.3000.7.2.17.0-Update3.postgres.sql
@@ -1,8 +1,22 @@
 SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.17.0-Update2 to 3.1.3000.7.2.17.0-Update3';
 
--- HIVE-26221
-ALTER TABLE "TAB_COL_STATS" ADD "HISTOGRAM" bytea;
-ALTER TABLE "PART_COL_STATS" ADD "HISTOGRAM" bytea;
+-- HIVE-27457
+UPDATE "SDS"
+    SET "INPUT_FORMAT" = 'org.apache.hadoop.hive.kudu.KuduInputFormat', "OUTPUT_FORMAT" = 'org.apache.hadoop.hive.kudu.KuduOutputFormat'
+    WHERE "SD_ID" IN (
+        SELECT "SD_ID" FROM "TBLS"
+            INNER JOIN "TABLE_PARAMS" ON "TBLS"."TBL_ID" = "TABLE_PARAMS"."TBL_ID"
+                WHERE "PARAM_VALUE" LIKE '%KuduStorageHandler%'
+        );
+
+UPDATE "SERDES"
+    SET "SLIB" = 'org.apache.hadoop.hive.kudu.KuduSerDe'
+    WHERE "SERDE_ID" IN (
+        SELECT "SDS"."SERDE_ID"
+            FROM "TBLS"
+            INNER JOIN "SDS" ON "TBLS"."SD_ID" = "SDS"."SD_ID"
+            WHERE "TBL_ID" IN (SELECT "TBL_ID" FROM "TABLE_PARAMS" WHERE "PARAM_VALUE" LIKE '%KuduStorageHandler%')
+    );
 
 -- These lines need to be last.  Insert any changes above.
 UPDATE "CDH_VERSION" SET "SCHEMA_VERSION"='3.1.3000.7.2.17.0-Update3', "VERSION_COMMENT"='Hive release version 3.1.3000 for CDH 7.2.17.0-Update3' where "VER_ID"=1;
