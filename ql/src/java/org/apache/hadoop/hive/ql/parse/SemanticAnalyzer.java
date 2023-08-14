9ca5d3ef13de0e79a78ffb0c80d11dab5f488a61
kkasa <kkasa@cloudera.com>
2023-08-14 11:12:29 +0200

    CDPD-59678: HIVE-27527: Order of records is not ensured in delete delta files when reduce deduplication is off (Krisztian Kasa, reviewed by Denys Kuzmenko)
    Change-Id: I903dc716b76f4f4094937b97d33e448145ffee59

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7128,7 +7128,6 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     boolean enforceBucketing = false;
     List<ExprNodeDesc> partnCols = new ArrayList<>();
     List<ExprNodeDesc> sortCols = new ArrayList<>();
-    List<Integer> sortOrders = new ArrayList<>();
     boolean multiFileSpray = false;
     int numFiles = 1;
     int totalFiles = 1;
@@ -7152,6 +7151,8 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       // Non-native acid tables should handle their own bucketing for updates/deletes
       if ((updating(dest) || deleting(dest)) && !AcidUtils.isNonNativeAcidTable(dest_tab, true)) {
         partnCols = getPartitionColsFromBucketColsForUpdateDelete(input, true);
+        sortCols = getPartitionColsFromBucketColsForUpdateDelete(input, false);
+        createSortOrderForUpdateDelete(sortCols, order, nullOrder);
         enforceBucketing = true;
       }
     }
