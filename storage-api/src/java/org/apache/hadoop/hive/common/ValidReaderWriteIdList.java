0d03dc7207fe762214db0663611757d28f3b76bd
Denys Kuzmenko <deniskuzZ@gmail.com>
2023-10-02 14:50:51 +0300

    CDPD-94592: HIVE-27526: Cleaner should honor compaction writeIdHwm (Denys Kuzmenko, reviewed by Laszlo Vegh, Sourabh Badhya)
    Closes #4491
    
    (cherry picked from commit de49826286e57976be447c00f9b9abfc41bc305e)

--- storage-api/src/java/org/apache/hadoop/hive/common/ValidReaderWriteIdList.java
+++ storage-api/src/java/org/apache/hadoop/hive/common/ValidReaderWriteIdList.java
@@ -257,8 +257,8 @@ public class ValidReaderWriteIdList implements ValidWriteIdList {
     }
   }
 
-  public ValidReaderWriteIdList updateHighWatermark(long value) {
-    return new ValidReaderWriteIdList(tableName, exceptions, abortedBits, value, minOpenWriteId);
+  public void setHighWatermark(long value) {
+    this.highWatermark = value;
   }
 }
 
