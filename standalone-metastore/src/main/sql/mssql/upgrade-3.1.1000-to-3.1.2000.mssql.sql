437b23dce439b8b321914b94222dbc802023cb5c
Stamatis Zampetakis <zabetak@cloudera.com>
2023-10-19 13:55:05 +0200

    CDPD-62662: Metastore schema upgrade from 3.1.1000 onwards fails in MSSQL
    The ALTER statement command fails while attempting to ADD new column to
    the table.
    ```
    Error: Incorrect syntax near the keyword 'COLUMN'. (state=S0001,code=156)
    ```
    The ADD COLUMN syntax that is used to do this is not valid in Microsoft
    SQL Server. Using the COLUMN keyword in the ALTER statement is incorrect
    and must be removed.
    
    Change-Id: Id2869c8897bd4f1815446de6cdb93cf29fa86072

--- standalone-metastore/src/main/sql/mssql/upgrade-3.1.1000-to-3.1.2000.mssql.sql
+++ standalone-metastore/src/main/sql/mssql/upgrade-3.1.1000-to-3.1.2000.mssql.sql
@@ -1,10 +1,10 @@
 SELECT 'Upgrading MetaStore schema from 3.1.1000 to 3.1.2000';
 
 ALTER TABLE TAB_COL_STATS
-ADD COLUMN ENGINE character varying(128) NOT NULL;
+ADD "ENGINE" character varying(128) NOT NULL;
 
 ALTER TABLE PART_COL_STATS
-ADD COLUMN ENGINE character varying(128) NOT NULL;
+ADD "ENGINE" character varying(128) NOT NULL;
 
 
 -- These lines need to be last.  Insert any changes above.
