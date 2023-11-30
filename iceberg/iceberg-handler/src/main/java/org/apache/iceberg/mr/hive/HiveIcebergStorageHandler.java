1db03970493fc1450819fbd94ffb80327581e2bc
Ayush Saxena <ayushsaxena@apache.org>
2023-11-30 15:44:54 +0530

    CDPD-62265: HIVE-27903: Iceberg: Implement Expire Snapshot with default table properties. (#4906). (Ayush Saxena, reviewed by zhangbutao)
    Change-Id: I2e1842e23fff6bdd9d18ae89e96253418bc356d1

--- iceberg/iceberg-handler/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergStorageHandler.java
+++ iceberg/iceberg-handler/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergStorageHandler.java
@@ -867,7 +867,9 @@ public class HiveIcebergStorageHandler implements HiveStoragePredicateHandler, H
         LOG.info("Executing expire snapshots on iceberg table {} with {} threads", icebergTable.name(), numThreads);
         deleteExecutorService = getDeleteExecutorService(icebergTable.name(), numThreads);
       }
-      if (expireSnapshotsSpec.isExpireByTimestampRange()) {
+      if (expireSnapshotsSpec == null) {
+        expireSnapshotWithDefaultParams(icebergTable, deleteExecutorService);
+      } else if (expireSnapshotsSpec.isExpireByTimestampRange()) {
         expireSnapshotByTimestampRange(icebergTable, expireSnapshotsSpec.getFromTimestampMillis(),
             expireSnapshotsSpec.getTimestampMillis(), deleteExecutorService);
       } else if (expireSnapshotsSpec.isExpireByIds()) {
@@ -882,6 +884,14 @@ public class HiveIcebergStorageHandler implements HiveStoragePredicateHandler, H
         deleteExecutorService.shutdown();
       }
     }
+  }
+
+  private void expireSnapshotWithDefaultParams(Table icebergTable, ExecutorService deleteExecutorService) {
+    ExpireSnapshots expireSnapshots = icebergTable.expireSnapshots();
+    if (deleteExecutorService != null) {
+      expireSnapshots.executeDeleteWith(deleteExecutorService);
+    }
+    expireSnapshots.commit();
   }
 
   private void expireSnapshotRetainLast(Table icebergTable, int numRetainLast, ExecutorService deleteExecutorService) {
