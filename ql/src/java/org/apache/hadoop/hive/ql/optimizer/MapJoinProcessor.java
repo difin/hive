c2015ec44c2f7afecef5368b152329ccfb02dbf3
Stamatis Zampetakis <zabetak@gmail.com>
2023-12-07 17:56:08 +0100

    CDPD-64443: HIVE-27658: Error resolving join keys during conversion to dynamic partition hashjoin (Stamatis Zampetakis reviewed by Denys Kuzmenko)
    Sometimes when the compiler attempts to convert a Join to a
    Dynamic Partition HashJoin (DPHJ) and certain assumptions about the
    shape of the plan do not hold a SemanticException is thrown.
    
    The DPHJ is a performance optimization so there is no reason to raise a
    fatal error when the conversion cannot be performed. It is preferable
    to simply skip the conversion and use a regular join instead of
    blocking completely the query. The `MapJoinProcessor.getMapJoinDesc`
    method already returns null in certain cases, so it is safe to add
    another exit condition.
    
    Overview of changes:
    1. Return null when join key resolution fails and simply skip conversion
    to DPHJ.
    2. Log a warning instead of throwing a fatal SemanticException.
    3. Enrich error message with more information to improve diagnosability.
    
    Bringing the plan into a shape that will allow the DPHJ conversion is
    still meaningful but can be tracked independently with other tickets.
    
    Close apache/hive#4930
    
    Plan stats are slightly different in tez_dynpart_hashjoin_4.q.out but
    the error is still reproable and test valid.
    
    Change-Id: I471a1dd1af8fca6ceab9dbb47acbaa1743678fee

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/MapJoinProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/MapJoinProcessor.java
@@ -1338,7 +1338,8 @@ public class MapJoinProcessor extends Transform {
         List<ExprNodeDesc> keyExprList =
             ExprNodeDescUtils.resolveJoinKeysAsRSColumns(mapEntry.getValue(), rsParent);
         if (keyExprList == null) {
-          throw new SemanticException("Error resolving join keys");
+          LOG.warn("Error resolving join keys {} in {} {}", mapEntry.getValue(), rsParent, rsParent.getColumnExprMap());
+          return null;
         }
         newKeyExprMap.put(pos, keyExprList);
       }
