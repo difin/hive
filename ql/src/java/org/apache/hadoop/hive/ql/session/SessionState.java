d35c811946e84b26904d5724134c07e20d6f9ed0
Mayank Kunwar <mkunwar@cloudera.com>
2023-12-15 15:35:51 +0530

    CDPD-64723: Backport HIVE-24858: UDFClassLoader leak in Configuration.CACHE_CLASSES (#2049) (Karen Coppage, reviewed by Peter Vary)
    Change-Id: Ic89575636fec13c63ff5392371c074b6bb738d8c

--- ql/src/java/org/apache/hadoop/hive/ql/session/SessionState.java
+++ ql/src/java/org/apache/hadoop/hive/ql/session/SessionState.java
@@ -1919,6 +1919,7 @@ public class SessionState {
       txnMgr.closeTxnManager();
     }
     JavaUtils.closeClassLoadersTo(sessionConf.getClassLoader(), parentLoader);
+    Utilities.restoreSessionSpecifiedClassLoader(getClass().getClassLoader());
     File resourceDir =
         new File(getConf().getVar(HiveConf.ConfVars.DOWNLOADED_RESOURCES_DIR));
     LOG.debug("Removing resource dir " + resourceDir);
