2ef3de32db8cc84164c6fe7d68b930c9b1a31471
Bodor Laszlo <bodorlaszlo0202@gmail.com>
2024-04-25 20:40:42 +0200

    CDPD-69227 HIVE-28212: MiniHS2: use a base folder which is more likely writable on the local FS (#5212) (Laszlo Bodor reviewed by Simhadri Govindappa)
    Change-Id: I9a0184c0c485001418b14d2f965f9ac752771171

--- itests/util/src/main/java/org/apache/hive/jdbc/miniHS2/MiniHS2.java
+++ itests/util/src/main/java/org/apache/hive/jdbc/miniHS2/MiniHS2.java
@@ -300,7 +300,7 @@ public class MiniHS2 extends AbstractHiveService {
       }
       // store the config in system properties
       mr.setupConfiguration(getHiveConf());
-      baseFsDir = new Path(new Path(fs.getUri()), "/base");
+      baseFsDir = new Path(new Path(fs.getUri()), HiveConf.ConfVars.SCRATCHDIR.getDefaultValue());
     } else {
       // This is FS only mode, just initialize the dfs root directory.
       fs = FileSystem.getLocal(hiveConf);
