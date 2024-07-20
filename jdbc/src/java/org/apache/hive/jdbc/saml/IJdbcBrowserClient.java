aba2c47e9d5e014d815a857931ea17ccd62c98b1
dengzh <dengzhhu653@gmail.com>
2024-07-20 09:12:34 +0800

    HIVE-28315: Missing classes while using hive jdbc standalone jar (#5313) (Zhihua Deng, reviewed by Denys Kuzmenko)

--- jdbc/src/java/org/apache/hive/jdbc/saml/IJdbcBrowserClient.java
+++ jdbc/src/java/org/apache/hive/jdbc/saml/IJdbcBrowserClient.java
@@ -19,7 +19,6 @@
 package org.apache.hive.jdbc.saml;
 
 import com.google.common.base.Preconditions;
-import com.google.errorprone.annotations.Immutable;
 import java.io.Closeable;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -111,7 +110,6 @@ public interface IJdbcBrowserClient extends Closeable {
   /**
    * Util class which can be used to parse the response received from the server.
    */
-  @Immutable
   class HiveJdbcBrowserServerResponse {
     private final String msg;
     private final boolean status;
