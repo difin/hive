ce7d04b5fbeba7f9c8fe06b4d0827c04b216d9c0
rtrivedi <rtrivedi@cloudera.com>
2025-06-17 06:46:53 -0500

    CDPD-84544: CDPD-63044: HIVE-27829 : New command "Show Processlist" to display current operations and related details in Hiveserver2 Change-Id: I87dd73453ce8a639977ee1ad48b49da8abb794b5
    Change-Id: I46b7560cf3a0dcd9a63897c6869d7b1823cfef4c

--- service/src/test/org/apache/hive/service/cli/operation/TestHiveCommandOpForProcessList.java
+++ service/src/test/org/apache/hive/service/cli/operation/TestHiveCommandOpForProcessList.java
@@ -20,7 +20,6 @@ package org.apache.hive.service.cli.operation;
 import com.google.common.collect.ImmutableMap;
 import org.apache.hadoop.hive.common.io.SessionStream;
 import org.apache.hadoop.hive.conf.HiveConf;
-import org.apache.hadoop.hive.conf.HiveConfForTest;
 import org.apache.hadoop.hive.ql.processors.ShowProcessListProcessor;
 import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hive.service.cli.HiveSQLException;
@@ -47,7 +46,7 @@ public class TestHiveCommandOpForProcessList {
 
   @Before
   public void setupTest() throws Exception {
-    hiveConf = new HiveConfForTest(getClass());
+    hiveConf = new HiveConf(getClass());
     hiveConf.setBoolVar(HiveConf.ConfVars.HIVE_SUPPORT_CONCURRENCY, false);
     hiveConf.setVar(HiveConf.ConfVars.HIVE_AUTHORIZATION_MANAGER,
         "org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdHiveAuthorizerFactory");
