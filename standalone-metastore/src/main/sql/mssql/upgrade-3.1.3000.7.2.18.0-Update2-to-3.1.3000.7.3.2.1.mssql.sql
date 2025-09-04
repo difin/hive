0ee97b093057d9fee91a0a390829c09d99e5b000
Zsolt Miskolczi <zsmiskolczi@cloudera.com>
2025-09-04 09:30:55 +0200

    CDPD-89372: Schema scripts for 7.3.2.1

---
+++ standalone-metastore/src/main/sql/mssql/upgrade-3.1.3000.7.2.18.0-Update2-to-3.1.3000.7.3.2.1.mssql.sql
@@ -0,0 +1,5 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.2.18.0-Update2 to 3.1.3000.7.3.2.1' AS MESSAGE;
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.3.2.1', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.3.2.1' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.2.18.0-Update2 to 3.1.3000.7.3.2.1' AS MESSAGE;
