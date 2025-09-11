a691fe140126295c7be31c0ae5c60f06ccfc1d41
rkirti <rkirti@cloudera.com>
2025-09-11 22:11:37 +0530

    Revert "CDPD-79722: HIVE-28417: Bump Log4j2 to 2.24.3(#5636) (Ling Hengqian, Zsolt Miskolczi，reviewed by Raghav Aggarwal, Butao Zhang) (#5636)"
    This reverts commit 2f456c520365b737473d10ac57028024e17f7097.

--- llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/QueryTracker.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/QueryTracker.java
@@ -48,9 +48,9 @@ import org.apache.tez.common.security.JobTokenIdentifier;
 import org.apache.tez.common.security.TokenCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.slf4j.Marker;
-import org.slf4j.impl.StaticMarkerBinder;
+import org.slf4j.MDC;
+import org.slf4j.MarkerFactory;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -70,8 +70,8 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class QueryTracker extends AbstractService {
 
   private static final Logger LOG = LoggerFactory.getLogger(QueryTracker.class);
-  private static final Marker QUERY_COMPLETE_MARKER =
-      new Log4jMarker(StaticMarkerBinder.getSingleton().getMarkerFactory(), new Log4jQueryCompleteMarker());
+
+  private static final Marker QUERY_COMPLETE_MARKER = MarkerFactory.getMarker("EOF_MARKER");
 
   /// Shared singleton MetricsSource instance for all DAG locks
   private static final MetricsSource LOCK_METRICS;
