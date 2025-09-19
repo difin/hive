58e8ee7252e9ceae50b894004d3d88220b61ae9d
saihemanth <saihemanth@cloudera.com>
2025-09-19 14:30:43 -0700

    CDPD-90564: Update HMS Backend-DB schema files for 7.3.2.0 (#1854)
    Change-Id: I4168bcccf83f11e21796d28ea9b86d863202e3a6

--- standalone-metastore/src/main/sql/derby/upgrade-3.1.3000.7.2.18.0-Update2-to-3.1.3000.7.3.2.1.derby.sql
+++
@@ -1,4 +0,0 @@
--- Upgrade MetaStore schema from 3.1.3000.7.2.18.0-Update2-to-3.1.3000.7.3.2.1
-
--- These lines need to be last.  Insert any changes above.
-UPDATE "APP".CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.3.2.1', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.3.2.1' where VER_ID=1;
