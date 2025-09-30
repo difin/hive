36926f3c3dd33b816f105f0f3bec32e11a60c44b
jintongjiang <jintong.jiang@cloudera.com>
2025-09-30 13:07:47 -0700

    CDPD-90351:HIVE-29202: Populate metastore authz context with client metadata (#1886)

--- ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/metastore/HiveMetaStoreAuthorizableEvent.java
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/metastore/HiveMetaStoreAuthorizableEvent.java
@@ -19,15 +19,19 @@
 
 package org.apache.hadoop.hive.ql.security.authorization.plugin.metastore;
 
+import org.apache.hadoop.hive.metastore.HiveMetaStore;
 import org.apache.hadoop.hive.metastore.api.DataConnector;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.events.PreEventContext;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthzContext;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HivePrivilegeObject;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /*
 HiveMetaStoreAuthorizableEvent: Abstract class for getting the MetaStore Event context for HiveMetaStore Authorization
@@ -39,6 +43,32 @@ public abstract class HiveMetaStoreAuthorizableEvent {
   protected HiveMetaStoreAuthorizableEvent(PreEventContext preEventContext) {
     this.preEventContext = preEventContext;
   }
+
+  protected HiveAuthzContext buildAuthzContext(String commandString) {
+    HiveAuthzContext.Builder builder = new HiveAuthzContext.Builder();
+
+    if (commandString != null) {
+      builder.setCommandString(commandString);
+    }
+
+    // TODO: refer to SessionManager/HiveSessionImpl for details on getting ipAddress and forwardedAddresses
+    builder.setForwardedAddresses(new ArrayList<>());
+
+    String ipAddress = HiveMetaStore.HMSHandler.getIPAddress();
+    builder.setUserIpAddress(ipAddress);
+
+    Map<String, Object> clientConfig = HiveMetaStoreAuthorizer.getClientConfig();
+    if (clientConfig != null) {
+      builder.setClientConfig(clientConfig);
+    }
+
+    return builder.build();
+  }
+
+  protected HiveAuthzContext buildAuthzContext() {
+    return buildAuthzContext(null);
+  }
+
 
   public abstract HiveMetaStoreAuthzInfo getAuthzContext();
 
--- ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/metastore/events/ReadDatabaseEvent.java
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/metastore/events/ReadDatabaseEvent.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.hadoop.hive.metastore.events.PreEventContext;
 import org.apache.hadoop.hive.metastore.events.PreReadDatabaseEvent;
+import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveAuthzContext;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveOperationType;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HivePrivilegeObject;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.metastore.HiveMetaStoreAuthorizableEvent;
@@ -44,9 +45,13 @@ public class ReadDatabaseEvent extends HiveMetaStoreAuthorizableEvent {
 
     @Override
     public HiveMetaStoreAuthzInfo getAuthzContext() {
-        HiveMetaStoreAuthzInfo ret = new HiveMetaStoreAuthzInfo(preEventContext, HiveOperationType.SHOWDATABASES, getInputHObjs(), getOutputHObjs(), COMMAND_STR);
-
-        return ret;
+      List<HivePrivilegeObject> inputHObjs = getInputHObjs();
+      List<HivePrivilegeObject> outputHObjs = getOutputHObjs();
+      HiveAuthzContext authzContext = buildAuthzContext(COMMAND_STR);
+      HiveMetaStoreAuthzInfo ret = new HiveMetaStoreAuthzInfo(preEventContext, HiveOperationType.SHOWDATABASES,
+          inputHObjs, outputHObjs, COMMAND_STR, authzContext);
+      LOG.debug("ReadDatabaseEvent.getAuthzContext(): authzContext=" + authzContext);
+      return ret;
     }
 
     private List<HivePrivilegeObject> getInputHObjs() {
