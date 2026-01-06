b8e401c4cbc97ecb8a93ca4581412762a68f0822
zdeng <zdeng@cloudera.com>
2026-01-06 12:45:33 +0800

    CDPD-94256: Expand the COMMENT column for COLUMNS_V2 table in HMS schema

---
+++ standalone-metastore/src/main/sql/mysql/upgrade-3.1.3000.7.3.2.0-Update1-to-3.1.3000.7.3.2.0-Update2.mysql.sql
@@ -0,0 +1,7 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.3.2.0-Update1 to 3.1.3000.7.3.2.0-Update2' AS MESSAGE;
+
+ALTER TABLE `COLUMNS_V2` MODIFY COLUMN `COMMENT` varchar(4000) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL, ALGORITHM=INPLACE, LOCK=NONE;
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.3.2.0-Update2', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.3.2.0-Update2' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.3.2.0-Update1 to 3.1.3000.7.3.2.0-Update2';
---
+++ standalone-metastore/src/main/sql/oracle/upgrade-3.1.3000.7.3.2.0-Update1-to-3.1.3000.7.3.2.0-Update2.oracle.sql
@@ -0,0 +1,7 @@
+SELECT 'Upgrading MetaStore schema from 3.1.3000.7.3.2.0-Update1 to 3.1.3000.7.3.2.0-Update2' AS Status from dual;
+
+ALTER TABLE COLUMNS_V2 MODIFY ("COMMENT" VARCHAR2(4000));
+
+-- These lines need to be last.  Insert any changes above.
+UPDATE CDH_VERSION SET SCHEMA_VERSION='3.1.3000.7.3.2.0-Update2', VERSION_COMMENT='Hive release version 3.1.3000 for CDH 7.3.2.0-Update2' where VER_ID=1;
+SELECT 'Finished upgrading MetaStore schema from 3.1.3000.7.3.2.0-Update1 to 3.1.3000.7.3.2.0-Update2' AS Status from dual;
